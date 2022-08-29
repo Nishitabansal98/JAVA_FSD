b8e3b0815a07b2cc318ff40247819c034c46954a
nishita24011998 <nishita.cairsc.mathura@gmail.com>
2022-08-29 23:19:45 +0530

    changes

--- Phase3PracticeProject3/src/test/java/com/example/UserManager/Phase3PracticeProject3ApplicationTests.java
+++ Phase3PracticeProject3/src/test/java/com/example/UserManager/Phase3PracticeProject3ApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class Phase3PracticeProject3ApplicationTests {
+class UserManagerApplicationTests {
 
 	@Test
 	void contextLoads() {
